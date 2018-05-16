f887623b66d71a59a9b9695e163cab74242f3746
Ramit Mitra <ramit.mitra@gmail.com>
2018-05-17 02:47:14 +0530

    Harmonizing code

--- module/welcome.js
+++ module/welcome.js
@@ -1,11 +1,11 @@
 module.exports = function (DIR) {
-    console.log('\033[1;35m--------------------------------------------------------');
-    console.log('\033[1;35mCODE WATCHDOG v1.0');
-    console.log('\033[1;35m--------------------------------------------------------');
-    console.log('\033[1;33mAutomated watchdog framework, extendable to run tools on code changes');
-    console.log('\033[1;32mAuthor : Ramit Mitra');
-    console.log('========================================================');
-    console.log('\033[1;31m✓ Started watching files at \033[1;32m' + DIR);
-    console.log('========================================================');
-    console.log('\x1b[37m');
-}
+	console.log("\033[1;35m--------------------------------------------------------");
+	console.log("\033[1;35mCODE WATCHDOG v1.0");
+	console.log("\033[1;35m--------------------------------------------------------");
+	console.log("\033[1;33mAutomated watchdog framework, extendable to run tools on code changes");
+	console.log("\033[1;32mAuthor : Ramit Mitra");
+	console.log("========================================================");
+	console.log("\033[1;31m✓ Started watching files at \033[1;32m" + DIR);
+	console.log("========================================================");
+	console.log("\x1b[37m");
+};
--- validator/config.js
+++ validator/config.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+var fs = require("fs");
+var shell = require("shelljs");
 
 module.exports = function (DIR) {
-    if (!fs.existsSync(DIR)) {
-        /* directory parameter in config file points to a directory that's not present */
-        console.log('\033[1;31mDirectory mentioned in config file doesnot exist !!!');
-        console.log('\033[1;31mEnsure the directory(dir) parameter is set to a valid directory');
-        shell.exit(1);
-    }
-}
+	if (!fs.existsSync(DIR)) {
+		/* directory parameter in config file points to a directory that's not present */
+		shell.echo("\033[1;31mDirectory mentioned in config file doesnot exist !!!");
+		shell.echo("\033[1;31mEnsure the directory(dir) parameter is set to a valid directory");
+		shell.exit(1);
+	}
+};
